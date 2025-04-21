b8ec092ea4b8f4c72320dbff0269b91e57e559ad
Samm Caagbay <sammcaag@gmail.com>
2025-04-22 02:14:08 +0800

    Cut: Remove Redirecting Route

--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,11 @@ import { Button } from "~/components/ui/button";
 import { useState } from "react";
 
 export default function SplashScreen() {
-  const [user, setUser] = useState(false);
+  // const [user, setUser] = useState(false);
 
-  if (!user) {
-    return <Redirect href="/home" />;
-  }
+  // if (!user) {
+  //   return <Redirect href="/home" />;
+  // }
 
   return (
     <View className="items-center justify-center flex-1 bg-black">

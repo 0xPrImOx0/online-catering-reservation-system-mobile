89ffda5309622cbfd785cc69570a71f92b64e189
Rey Daug <daug.rey34@gmail.com>
2025-04-27 02:27:34 +0800

    chore: update dependencies in package.json to include expo-dev-client

--- lib/icons/iconWithClassName.ts
+++ lib/icons/iconWithClassName.ts
@@ -1,22 +1,10 @@
-import type { LucideIcon } from 'lucide-react-native';
-import { cssInterop } from 'nativewind';
+import type { LucideIcon } from "lucide-react-native";
+import { cssInterop } from "nativewind";
 
 export function iconWithClassName(icon: LucideIcon) {
-<<<<<<< HEAD
-cssInterop(icon, {
-  className: {
-    target: 'style',
-    nativeStyleToProp: {
-      color: true,
-      opacity: true,
-    },
-  },
-});
-}
-=======
   cssInterop(icon, {
     className: {
-      target: 'style',
+      target: "style",
       nativeStyleToProp: {
         color: true,
         opacity: true,
@@ -24,4 +12,3 @@ cssInterop(icon, {
     },
   });
 }
->>>>>>> 593890f17d083bdfa339c0c91163757a0212a69c

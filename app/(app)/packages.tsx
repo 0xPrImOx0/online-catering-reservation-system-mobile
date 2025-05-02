bab1f106c5b7be70971d54e4f0ece5a42125fe58
Rey Daug <daug.rey34@gmail.com>
2025-05-02 14:01:17 +0800

    feat: 🆕 Implement API call to fetch catering packages and update state management fix: 🛠️ Correct filter logic to handle empty catering packages array refactor: ✨ Clean up imports and improve error handling for API requests

--- app/(app)/packages.tsx
+++ app/(app)/packages.tsx
@@ -1,20 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View, FlatList } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "~/lib/useColorScheme";
 import CategoryPill from "~/components/menus/CategoryPill";
-import { cateringPackages, packages } from "~/lib/packages-metadata";
+import { packages } from "~/lib/packages-metadata";
 import PackageCard from "~/components/packages/PackageCard";
+import api from "~/lib/axiosInstance";
+import axios from "axios";
+import { CateringPackagesProps } from "~/types/package-types";
 
 export default function PackagesPage() {
   const { isDarkColorScheme } = useColorScheme();
+  const [cateringPackages, setCateringPackages] = useState<
+    CateringPackagesProps[]
+  >([]);
+  const [selectedType, setSelectedType] = useState("All");
+
+  useEffect(() => {
+    const getPackages = async () => {
+      try {
+        const response = await api.get("/packages");
+        setCateringPackages(response.data.data);
+      } catch (err: unknown) {
+        console.log("ERRRORRR", err);
+
+        if (axios.isAxiosError<{ error: string }>(err)) {
+          const message = err.response?.data.error || "Unexpected Error Occur";
+
+          console.error("ERROR FETCHING PACKAGES", message);
+        } else {
+          console.error("Something went wrong. Please try again.");
+        }
+      }
+    };
+
+    getPackages();
+  }, []);
+
   const filterPackages = (type: string) => {
+    if (!cateringPackages) return [];
+
     if (type === "All") {
       return cateringPackages;
     }
+
     return cateringPackages.filter((pkg) => pkg.eventType === type);
   };
-  const [selectedType, setSelectedType] = useState("All");
 
   return (
     <View className="flex-1 bg-background">
